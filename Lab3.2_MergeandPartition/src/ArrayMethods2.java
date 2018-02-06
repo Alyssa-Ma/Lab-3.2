4b7fbb235ed278127019f22fbf044bb464ad4dd0
Alyssa <Alyssa@192.168.1.151>
2018-02-06 00:08:56 -0500

    start

---
+++ Lab3.2_MergeandPartition/src/ArrayMethods2.java
@@ -0,0 +1,43 @@
+/**
+ * @author Alyssa
+ * Period 2 
+ * 2/15/18 
+ * Lab 3.2
+ */
+
+public class ArrayMethods2 
+{
+	public static String[] merge(String[] list2)
+	{
+		String[] f1;
+		String[] f2;
+		String[] newString;
+
+		while(f1 > 0; f2>f2)	
+		{
+			
+		}
+		
+
+
+
+
+
+
+	}
+
+
+	public static int partition(int[] list)
+	{
+		int left = indexof();
+		int right = indexof();
+
+		while( )
+		{
+			
+		}
+
+
+
+	}
+}
