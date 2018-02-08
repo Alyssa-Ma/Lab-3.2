b489ba85e211b1425e5d0440350424e049768ba4
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-02-08 09:23:08 -0500

    sad

--- Lab3.2_MergeandPartition/src/ArrayMethods2.java
+++ Lab3.2_MergeandPartition/src/ArrayMethods2.java
@@ -10,27 +10,13 @@ public class ArrayMethods2
 
 	public static void main(String [] args)
 	{
-		//Test arrays
-		int[]test1 = {1,4,4,5,6,4,3,17,0};
-		double[]test2 = {1.1,4.1,4.2,5.0,2.0,4.0,3.0,17.0,0.0};
-		String[]test3 = {"zebra", "tortilla", "abba", "foo", "bar", "aba"};
+		//Testing arrays
+		String [] test1 = {"apple", "cucumber", "microsoft", "zorro"};
+		String [] test2 = {"banana", "cherry", "mahogany", "oreos", "pina"};
+		int [] test3 = {3, 4, 2, 7, 12, 22, 0, 1};
 		
-		//Merge sort
-		long start = System.nanoTime();
-		mergesort(test1);
-		long end = System.nanoTime();
-		long time = end - start;
-		System.out.println("Insertion sort time: " + time + " nanoseconds.");
-		System.out.println(Arrays.toString(test1));
 		
-		//Partition
-		start = System.nanoTime();
-		selectionSort(test2);
-		end = System.nanoTime();
-		time = end - start;
-		System.out.println("Selection sort time: " +time + " nanoseconds.");
-		System.out.println(Arrays.toString(test2));
-		
+	}
 	
 	public static String[] merge(String[] list1, String[] list2)
 	{
