1469f5952d277b9216d33b7b208e1c50db41f1be
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-02-09 09:21:49 -0500

    finish

--- Lab3.2_MergeandPartition/src/ArrayMethods2.java
+++ Lab3.2_MergeandPartition/src/ArrayMethods2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author Alyssa
  * Period 2 
@@ -12,34 +14,35 @@ public class ArrayMethods2
 	{
 		//Testing arrays
 		String [] test1 = {"apple", "cucumber", "microsoft", "zorro"};
-		String [] test2 = {"banana", "cherry", "mahogany", "oreos", "pina"};
+		String [] test2 = {"banana", "cherry", "mahogany", "oreos", "pie"};
 		int [] test3 = {3, 4, 2, 7, 12, 22, 0, 1};
 		
+		//Merge
+		long start = System.nanoTime();
+		String[] mergeResult = merge(test1,test2);
+		long end = System.nanoTime();
+		long time = end- start;
+		System.out.println("Merge time: " + time + " nanoseconds.");
+		System.out.println(Arrays.toString(mergeResult));
 		
+		//Partition
+		start = System.nanoTime();
+		int pivotFinalPos = partition(test3);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Partition time: " + time + " nanoseconds.");
+		System.out.println("Final Pivot Position: " + pivotFinalPos);
+		System.out.println(Arrays.toString(test3));
 	}
 	
 	public static String[] merge(String[] list1, String[] list2)
 	{
 		//precondition: both lists are sorted.
-		String[] f1;
-		String[] f2;
-		String[] newString;
-
-		//new test
-		this.list1[] = list2;
+		int x;
+		int y;
+		int z;
 		
-				
-				
-		while(f1 > 0; f2>f2)	
-		{
-			
-		}
-		
-
-
-
-
-
+		int midpt = list1;
 
 	}
 
@@ -47,9 +50,20 @@ public class ArrayMethods2
 	public static int partition(int[] list)
 	{
 		int x;
-		String
+		int y;
+		int piv = list[x];
+		
+		while(x >1)
+		{
+			
+		}
+		
+		
 
-
-
+	}
+	
+	public static int swap(int[] list)
+	{
+		list[] = 
 	}
 }
